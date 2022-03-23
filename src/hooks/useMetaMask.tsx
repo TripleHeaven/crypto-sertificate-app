8cad3b68cc2270c564061a5c8bcaf087dfe1b905
Ilya <heaven30.buroff@gmail.com>
2022-03-23 23:42:37 +0300

    init app and implement metamask wallet injector connection

---
+++ src/hooks/useMetaMask.tsx
@@ -0,0 +1,82 @@
+import { useWeb3React } from '@web3-react/core'
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
+import { injected } from '../constants/wallet'
+
+export const MetaMaskContext = createContext<any>(null)
+
+export const MetaMaskProvider: React.FC = ({ children }) => {
+  const { activate, account, library, connector, active, deactivate } =
+    useWeb3React()
+
+  const [isActive, setIsActive] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+
+  // first loading
+  useEffect(() => {
+    connect().then((val) => {
+      setIsLoading(false)
+    })
+  }, [])
+
+  const handleIsActive = useCallback(() => {
+    setIsActive(active)
+  }, [active])
+
+  useEffect(() => {
+    handleIsActive()
+  }, [handleIsActive])
+
+  // Connect to MetaMask wallet
+  const connect = async () => {
+    console.log('Connecting to MetaMask Wallet')
+    try {
+      await activate(injected)
+    } catch (error) {
+      console.log('Error on connecting: ', error)
+    }
+  }
+
+  // Disconnect from Metamask wallet
+  const disconnect = async () => {
+    console.log('Deactivating...')
+    try {
+      await deactivate()
+    } catch (error) {
+      console.log('Error on disconnecting: ', error)
+    }
+  }
+
+  const values = useMemo(
+    () => ({
+      isActive,
+      account,
+      isLoading,
+      connect,
+      disconnect,
+    }),
+    [isActive, isLoading]
+  )
+
+  return (
+    <MetaMaskContext.Provider value={values}>
+      {children}
+    </MetaMaskContext.Provider>
+  )
+}
+
+export const useMetaMask = () => {
+  const context = useContext<any>(MetaMaskContext)
+
+  if (context === undefined) {
+    throw new Error('provide MetaMask context')
+  }
+
+  return context
+}
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/yourfile.css',
+    }),
+    new NodePolyfillPlugin(),
+  ],
+}
